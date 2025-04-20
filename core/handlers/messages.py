9e823b8a1d84244a648b83b5ac8a7834d3056ec8
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2025-04-20 11:39:31 +0400

    Update messages.py

--- core/handlers/messages.py
+++ core/handlers/messages.py
@@ -1,27 +1,34 @@
 import asyncio
-import time
-
-from core.FSM.dialog_FSM import DialogReg
 from aiogram import F, Router, types
-from aiogram.types import Message, FSInputFile
+from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
 from core.database.models import async_session
 from core.middlwares.db import DBMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
-import re
+
 from core.utils.yndx_gpt import YandexGPTManager
 from core.handlers.logger import UserActionLogger
 
-
-action_logger = UserActionLogger()
+# Инициализация роутера, логгера и менеджера GPT
 dialog_router = Router()
 dialog_router.message.middleware(DBMiddleware(async_session))
+
 manager = YandexGPTManager()
+action_logger = UserActionLogger()
 
 
-@dialog_router.message()
-async def handle_message(message: Message):
+# Обработка только текстовых сообщений
+@dialog_router.message(F.text)
+async def handle_text_message(message: Message):
     chat_id = message.chat.id
-    response = await manager.send_message(chat_id, message.text)
+    user_input = message.text
+
+    response = await manager.send_message(chat_id, user_input)
     action_logger.log_action(message.from_user.id, 'answer')
     await message.answer(response)
+
+
+# Fallback — для всех остальных типов сообщений
+@dialog_router.message()
+async def handle_non_text_message(message: Message):
+    await message.answer("Я могу обработать только текст. Картинки обработаны не будут.")
