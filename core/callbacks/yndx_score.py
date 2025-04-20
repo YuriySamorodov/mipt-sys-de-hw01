081e30fe08340c469033738a331f7368e1faaf54
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2025-04-20 08:26:28 +0400

    Update yndx_score.py

--- core/callbacks/yndx_score.py
+++ core/callbacks/yndx_score.py
@@ -7,7 +7,7 @@ from core.middlwares.db import DBMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.database.models import async_session
 from aiogram import F, Router
-from aiogram import CallbackQuery
+from aiogram.types import CallbackQuery
 import re
 import os
 import json
