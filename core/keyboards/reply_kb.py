132ac4ae4430fac1ac4d7ab10825cd9bdd3d7ebb
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2025-04-20 07:08:43 +0400

    Update reply_kb.py

--- core/keyboards/reply_kb.py
+++ core/keyboards/reply_kb.py
@@ -1,11 +1,2 @@
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from aiogram.types.web_app_info import WebAppInfo
-
-
-main_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text='1')],
-        [KeyboardButton(text='2')]
-    ],
-    resize_keyboard=True,
-)
