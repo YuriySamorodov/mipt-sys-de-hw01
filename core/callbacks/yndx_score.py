df173a93dcc1ef4b09e1e044303aebcc7d993809
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2025-04-20 08:17:33 +0400

    Update yndx_score.py

--- core/callbacks/yndx_score.py
+++ core/callbacks/yndx_score.py
@@ -15,9 +15,15 @@ from dotenv import load_dotenv, set_key, get_key, dotenv_values
 import dotenv
 from aiogram.types import InputMediaPhoto, InputMediaVideo
 import asyncio
+from core.handlers.logger import UserActionLogger
 
-
+action_logger = UserActionLogger()
 clbs_router = Router()
 clbs_router.callback_query.middleware(DBMiddleware(async_session))
 
 # сюда позже добавим обработки клавиатуры для оценки ответов бота
+
+@clbs_router.callback_query()
+async def handle_callback(callback: types.CallbackQuery):
+    """Обработчик callback-запросов"""
+    action_logger.log_action(callback.from_user.id, 'callback')
