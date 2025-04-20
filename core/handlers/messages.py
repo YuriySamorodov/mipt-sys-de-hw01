74243eeb536b95faaa0808d3264d17731837d0dc
Yuriy Samorodov <Yuriy.Samorodov@gmail.com>
2025-04-20 08:13:38 +0400

    Update messages.py

--- core/handlers/messages.py
+++ core/handlers/messages.py
@@ -2,7 +2,7 @@ import asyncio
 import time
 
 from core.FSM.dialog_FSM import DialogReg
-from aiogram import F, Router
+from aiogram import F, Router, types
 from aiogram.types import Message, FSInputFile
 from aiogram.fsm.context import FSMContext
 from core.database.models import async_session
@@ -10,8 +10,10 @@ from core.middlwares.db import DBMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 import re
 from core.utils.yndx_gpt import YandexGPTManager
+from core.handlers.logger import UserActionLogger
 
 
+action_logger = UserActionLogger()
 dialog_router = Router()
 dialog_router.message.middleware(DBMiddleware(async_session))
 manager = YandexGPTManager()
@@ -21,4 +23,5 @@ manager = YandexGPTManager()
 async def handle_message(message: Message):
     chat_id = message.chat.id
     response = await manager.send_message(chat_id, message.text)
+    action_logger.log_action(message.from_user.id, 'answer')
     await message.answer(response)
